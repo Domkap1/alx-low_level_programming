bbdb619c870c3b50377d4e70809f77d34f79a49c
Domkap1 <domkappaul@gmail.com>
2023-07-19 18:14:36 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,6 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	
 	if (n >= 0 && n <= 15)
 	{
 	for (i = 0; i <= n; i++)
@@ -15,7 +14,6 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 		k = j * i;
-		
 		if (j == 0)
 		{
 			_putchar('0');
@@ -24,7 +22,6 @@ void print_times_table(int n)
 		{
 			_putchar(',');
 			_putchar(' ');
-			
 			if (k < 10)
 			{
 				_putchar(' ');
@@ -44,7 +41,6 @@ void print_times_table(int n)
 				_putchar((k % 10) + '0');
 			}
 		}
-		
 		}
 		_putchar('\n');
 	}
