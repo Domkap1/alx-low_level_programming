7182a116bdea885d7f2bdfc49a9f361dea25bf61
Domkap1 <domkappaul@gmail.com>
2023-07-25 03:47:17 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,9 @@
-#iinclude "main.h"
+#include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * _atoi - converts a string to an integer.
+ * @s: input string.
+ * Return: integer.
  */
 int _atoi(char *s)
 {
@@ -17,34 +16,31 @@ int _atoi(char *s)
 	f = 0;
 	digit = 0;
 
-	/* Calculate the length of the string */
-	while (s[len] != '\0len++;
+	while (s[len] != '\0')
+		len++;
 
 	while (i < len && f == 0)
 	{
-	if (s[i] == '-')
-		++d;
+		if (s[i] == '-')
+			++d;
 
-	if (s[i] >= '0' && s[i] <= '9')
-	{
-		digit = s[i] - '0';
-            if (d % 2)
-                digit = -digit;
-            if (n > INT_MAX / 10 || (n == INT_MAX / 10 && digit > 7))
-                return INT_MAX; // Overflow check
-            if (n < INT_MIN / 10 || (n == INT_MIN / 10 && digit < -8))
-                return INT_MIN; // Underflow check
-            n = n * 10 + digit;
-            f = 1;
-            if (s[i + 1] < '0' || s[i + 1] > '9')
-                break;
-            f = 0;
-        }
-        i++;
-    
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
 
-    if (f == 0)
-        return (0);
+	if (f == 0)
+		return (0);
 
-    return (n);
+	return (n);
 }
+
