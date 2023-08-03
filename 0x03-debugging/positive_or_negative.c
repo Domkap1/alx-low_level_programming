424ec73a930995a672f9f40225d2f20c71cc04de
Domkap1 <domkappaul@gmail.com>
2023-08-03 08:04:45 -0700

    0. Multiple mains

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,23 +9,23 @@
 
 int main(void)
 {
-        int i;
+		int i;
 
-        srand(time(0));
-        i = rand() - RAND_MAX / 2;
-        if (i > 0)
-        {
-                printf("%d is positive\n", i);
-        }
+		srand(time(0));
+		i = rand() - RAND_MAX / 2;
+		if (i > 0)
+		{
+			printf("%d is positive\n", i);
+		}
 
-        else if (i == 0)
-        {
-                printf("%d is zero\n", i);
-        }
-        else
-        {
-                printf("%d is negative\n", i);
-        }
-        return (0);
+		else if (i == 0)
+		{
+			printf("%d is zero\n", i);
+		}
+		else
+		{
+			printf("%d is negative\n", i);
+		}
+			return (0);
 }
 
